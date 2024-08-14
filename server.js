ba157ac662a9bf4cc69c3ceb544dc57da76d1c1e
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-14 16:54:58 -0600

    serve html content

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 "use strict";
 
 const http = require("node:http");
-
+const fs = require("node:fs/promises");
 // Sends a simple message to the client
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.write("hello there");
-    res.end();
+    // read the html file
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end("<h1>HI</h1>");
   }
+  if (req.url === "/html") {
+    fs.readFile(__dirname + "/index.html").then((content) => {
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.end(content);
+    });
+  }
+
   if (req.url === "/api/numbers") {
     res.write(JSON.stringify([1, 2, 3]));
     res.end();
   }
-});
-
-server.on("request", (req, res) => {
-  console.log("----- HEADERS -----");
-  console.log(req.headers);
-  console.log("----- BODY -----");
-  req.on("data", (chunk) => {
-    console.log(chunk.toString("utf-8"));
-  });
-  res.end();
 });
 
 server.listen(3000, () => {
